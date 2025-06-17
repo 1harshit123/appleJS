8631f8aa6dc442d8c5cefc6cec8a652ccc3fee41
1harshit123 <harshitkumar100436@gmail.com>
2025-06-18 01:02:28 +0530

    final commit

--- appleJS/apple/admin.py
+++ appleJS/apple/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from apple.models import Apple
 # Register your models here.
+
+admin.site.register(Apple)
--- appleJS/apple/models.py
+++ appleJS/apple/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Apple(models.Model):
+    email = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
+    date = models.DateField()
